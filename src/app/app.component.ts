a24c3caa4b7fdad804bbeeabf8cb2eba3ed8c117
Cyril Ajieh <cyril.ajieh@ibm.com>
2019-02-11 13:17:17 -0500

    First commit- scaffolding and render passengers list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+import { Passenger } from './passenger';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+   <main>
+   <section>
+   <header>
+   <h2>RailLine Passengers</h2>
+   </header>
+   <ul>
+    <li *ngFor="let passenger of passengers">
+    {{passenger.name}}
+    </li>
+   </ul>
+   </section>
+   </main>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'local-weather-app';
+
+  passengers: Passenger[] = [
+    {
+      "id": 167890,
+      "name": "Efosa",
+      "checkIn": true
+    },
+    {
+      "id": 222667,
+      "name": "Ehimen",
+      "checkIn": false
+    },
+    {
+      "id": 212343,
+      "name": "Isoken",
+      "checkIn": true
+    }
+  ]
 }
