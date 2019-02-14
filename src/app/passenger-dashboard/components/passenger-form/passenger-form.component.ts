278b987e760364a8296885ad303ef19ce6bcbc1d
Cyril Ajieh <cyril.ajieh@ibm.com>
2019-02-14 03:42:03 -0500

    Form control - select option

--- src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Passenger } from '../../models/passenger';
+import { Baggage } from '../../models/baggage';
 
 @Component({
     selector: 'passenger-form',
@@ -41,6 +42,16 @@ import { Passenger } from '../../models/passenger';
         No
       </label>
     </div>
+    <div>
+    <label>
+    <input 
+      type="checkbox"
+      name="checkedIn"
+      [ngModel]="detail?.checkedIn"
+      (ngModelChange)="toggleCheckIn($event)">
+      Check In:
+  </label>
+</div>
     <div *ngIf="form.value.checkedIn">
       Check in date:
       <input 
@@ -48,7 +59,38 @@ import { Passenger } from '../../models/passenger';
         name="checkInDate"
         [ngModel]="detail?.checkInDate">
     </div>
-
+  <div>
+  Luggage - using selected
+  <select 
+        name="baggage"
+        [ngModel]="detail?.baggage"
+        
+        >
+    <option
+         *ngFor="let bag of baggage"
+         [value]="bag.key"
+         [selected]="bag.key === detail?.baggage">
+         {{ bag.value }}
+    </option>
+  </select>
+  </div>
+  <div>
+  Luggage - using ngValue
+  <select 
+        name="baggage"
+        [ngModel]="detail?.baggage"
+        
+        >
+    <option
+         *ngFor="let bag of baggage"
+         [ngValue]="bag.key"
+         >
+         {{ bag.value }}
+    </option>
+  </select>
+  
+  
+  </div>
       {{ form.value | json }}
     </form>
     </section>
@@ -60,6 +102,22 @@ export class PassengerFormComponent {
 
     @Input()
     detail: Passenger;
+
+    baggage: Baggage[] = [{
+        key: 'none',
+        value: 'No baggage'
+    },
+    {
+        key: 'hand-only',
+        value: 'Hand baggage'
+    }, {
+        key: 'hold-only',
+        value: 'Hold baggage'
+    },
+    {
+        key: 'hand-hold',
+        value: 'Hand and hold baggage'
+    }];
 
     toggleCheckIn(checkedIn: boolean) {
         if (checkedIn) {
